38709556b460777078b04e94c770661585a5611b
SamaraParcero <samaraparceroti@gmail.com>
2023-10-02 21:14:52 -0300

    implementação na classe corrida para otimizar

--- CorridaDeCarros/Corrida.java
+++ CorridaDeCarros/Corrida.java
@@ -1,7 +1,7 @@
 package CorridaDeCarros;
 
 public class Corrida {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		
 		CodCarro carrinho = new CodCarro(0, 0);
 		CodCarro carrinho2 = new CodCarro(0, 0);
@@ -15,16 +15,23 @@ public class Corrida {
 		carrinho.setDistancia(carrinho.getDistancia() + carrinho.getVelocidade());
 		carrinho2.setDistancia(carrinho2.getDistancia() + carrinho2.getVelocidade());
 		pi.posicoesPista(carrinho, carrinho2);
+		
 		}
 	} catch (ArrayIndexOutOfBoundsException e) {
-		if (carrinho.getDistancia()> distanciaTotal.length) {
+		if (carrinho.getDistancia()>= distanciaTotal.length && carrinho.getDistancia() > carrinho2.getDistancia()) {
 			System.out.println("O carrinho 1 ultrapassou a linha de chegada e venceu!");
-		} else if (carrinho2.getDistancia()> distanciaTotal.length) {
+			System.out.println("Carrinho 1 ganhou com distância: " + carrinho.getDistancia());
+			System.out.println("Carrinho 2 perdeu com distância: " + carrinho2.getDistancia());
+		} else if (carrinho2.getDistancia()>= distanciaTotal.length && carrinho2.getDistancia() > carrinho.getDistancia()) {
 			System.out.println("O carrinho 2 ultrapassou a linha de chegada e venceu!");
-		} else {
+			System.out.println("Carrinho 1 perdeu com distância: " + carrinho.getDistancia());
+			System.out.println("Carrinho 2 ganhou com distância: " + carrinho2.getDistancia());
+		} else if (carrinho.getDistancia() == carrinho2.getDistancia()){
 			System.out.println("Os carrinhos ultrapassaram a linha de chegada juntos!");
+
 		}
 	 }
 		
 	}
+
 }
